084f478d8cb432c7a8f36bb107f087c30a71f046
Tomohiro Nakamura <naka.tomohiro@gmail.com>
2015-05-06 14:16:08 +0900

    Initial commit

---
+++ d-i/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  d-i
+//
+//  Created by Tomohiro Nakamura on 5/6/15.
+//  Copyright (c) 2015 tomohiron. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+
+		// Do any additional setup after loading the view.
+	}
+
+	override var representedObject: AnyObject? {
+		didSet {
+		// Update the view, if already loaded.
+		}
+	}
+
+
+}
+
